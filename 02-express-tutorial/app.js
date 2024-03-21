41e1dce27603b4f1fba1652cb03152e2ad723c08
amyzliao <amzhliao@gmail.com>
2024-03-21 17:15:41 -0500

    middleware!

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,51 +1,28 @@
 const express = require('express')
 const app = express();
-const { products, people } = require('./data')
+const morgan = require('morgan')
+const logger = require('./logger')
+const authorize = require('./authorize')
+
+// req => middleware => res
+// app.use([logger, authorize])
+// app.use(express.static('./public'))
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
-    // send response w param type JSON object
-    res.send('<h1>Home Page</h1><a href="/api/products">products</a>')
+    res.send('Home')
 })
-
+app.get('/about', (req, res) => {
+    res.send('About')
+})
 app.get('/api/products', (req, res) => {
-    const newProducts = products.map((product) => {
-        const { id, name, image } = product;
-        return { id, name, image }
-    })
-    res.json(newProducts);
+    res.send('Products')
+})
+app.get('/api/items', (req, res) => {
+    console.log(req.user)
+    res.send('Items')
 })
 
-app.get('/api/products/:productID', (req, res) => {
-    const { productID } = req.params;
-
-    const singleProduct = products.find(
-        (product) => product.id === Number(productID)
-    )
-    if (!singleProduct) {
-        return res.status(404).send('Product does not exist');
-    }
-    res.json(singleProduct);
-})
-
-app.get('/api/v1/query', (req, res) => {
-    console.log(req.query);
-    const { search, limit } = req.query;
-    let sortedProducts = [...products];
-    if (search) {
-        sortedProducts = sortedProducts.filter((product) => {
-            return product.name.startsWith(search)
-        })
-    }
-    if (limit) {
-        sortedProducts = sortedProducts.slice(0, Number(limit))
-    }
-    if (sortedProducts.length < 1) {
-        // dont send 404 bc resource and url exists. 
-        return res.status(200).json({success:true, data: sortedProducts})
-    }
-    // you cant send 2 responses to the same request
-    res.status(200).json(sortedProducts)
-})
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....')
